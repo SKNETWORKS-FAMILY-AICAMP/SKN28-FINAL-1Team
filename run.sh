ff8d3c0e247cd73985b72fbff9b0becd3313f3c5
92shepherd <shepherdpkw@gmail.com>
2026-07-15 14:08:40 +0900

    Fix: docker compose 실행 명령어

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-docker compose -f docker-compose.yml -f docker-compose.swagger.yml --profile all up -d --build
+docker compose \
+  -f docker-compose.yml \
+  -f docker-compose.override.yml \
+  -f docker-compose.swagger.yml \
+  --profile all \
+  up -d --build
